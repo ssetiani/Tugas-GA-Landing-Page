eec4888a17519ab9f786175d73607bb41c9a9327
ssetiani <fitriq@gmail.com>
2021-01-03 12:45:42 +0700

    add nav bar

---
+++ script.js
@@ -0,0 +1,14 @@
+// const menuToggle = document.querySelector('.menu-toggle input');
+// const nav = document.querySelector('nav ul');
+
+// menuToggle.addEventListener('click', function(){
+//     nav.classList.toggle('slide');
+// });
+
+
+const menuToggle = document.querySelector('.menu-toggle input');
+const nav = document.querySelector('nav ul');
+
+menuToggle.addEventListener('click',function() {
+    nav.classList.toggle('slide');
+});
